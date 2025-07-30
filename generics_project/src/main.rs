53e7a33186dfc78293c6798c1c44515a94608fae
anol123 <anol.roy123@gmail.com>
2025-07-30 18:29:39 +0530

    Adding the generics_project

--- generics_project/src/main.rs
+++ generics_project/src/main.rs
@@ -3,11 +3,46 @@ enum DigitalContent{
     AudioFile,
     Videofile
 }
+#[derive(Debug)]
 struct ChatMessage<T>{
     content: T,
     time: String
 }
+impl ChatMessage<DigitalContent>{
+    fn consume_entertainment(&self){
+        println!("{:?}",self.content);
+    }
+}
+impl<T> ChatMessage<T>{
+    fn retrieve_time(&self) -> String{
+        self.time.clone()
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+
+    let message =ChatMessage{
+        content:DigitalContent::AudioFile,
+        time: String::from("xyz")
+    };
+
+    message.consume_entertainment();
+    println!("{}",message.retrieve_time());
+
+    println!("{:#?}",message);
+
+    let message =ChatMessage{
+        content:"abcd",
+        time: String::from("20:20:55")
+    };
+    println!("{}",message.retrieve_time());
+
+    println!("{:#?}",message);
+
+    let message =ChatMessage{
+        content:"abcd".to_string(),
+        time: String::from("xyz")
+    };
+    println!("{}",message.retrieve_time());
+    println!("{:#?}",message);
 }
