1b85d22f27c5407c6be2a59e4aeb6a9731ae4c85
anol123 <anol.roy123@gmail.com>
2025-10-14 12:19:02 +0530

    Adding the learnings of closures

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -1,12 +1,23 @@
 fn main() {
-    let mut a =10;
-    let mut c ="abc".to_string();
-    let mut x  = ||{
-        let b=c;
-        a=a+1;
-        println!("{}",a);
+//     let mut a =10;
+//     let mut c ="abc".to_string();
+//     let mut x  = ||{
+//         let b=c;
+//         a=a+1;
+//         println!("{}",a);
 
-    };
-    x();
-   // println!("{}",c);  //error will come
+//     };
+//     x();
+//    // println!("{}",c);  //error will come
+
+
+
+let mut a =vec![2,3,4,5];
+
+let mut b = || a;
+b();
+// println!("{:?}",a);
+
+// println!("{:?}",a);
+
 }
